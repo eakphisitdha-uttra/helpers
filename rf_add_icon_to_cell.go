3e6a0124bcf5604c1ae305b31be9cb4a592e857f
Gantong <eakphisitdha.u@komgrip.com>
2024-07-24 18:14:57 +0700

    add test

--- rf_add_icon_to_cell.go
+++ rf_add_icon_to_cell.go
@@ -10,7 +10,7 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
-/* accept only .jpeg and .png*/
+// accept only .jpeg and .png*/
 
 func AddIconToCell(sheetName, filePath, column, row string, mergeCount int, f *excelize.File) error {
 
